d3fed66cb1d66bf74759c9a67536191362c0c518
cscsb <49179157+cscsb@users.noreply.github.com>
2020-03-12 18:18:49 +0800

    3

--- source/plugin/xiaomy_cus_todo/include/Todo.class.php
+++ source/plugin/xiaomy_cus_todo/include/Todo.class.php
@@ -13,9 +13,7 @@ class Todo
 	// 默认返回的数据
 	public $data = false;
 	// 允许访问的接口     加batch代码，允许访问api_batch.php文件
-
 	public $mods = ['batch','setMidOrders','catalog','getCatyitem', 'addItem', 'setItem', 'deleteItem', 'mvItem', 'getMenu', 'addMenu', 'deleteMenu', 'uploadImages', 'getImages', 'deleteImages', 'setItemOrders', 'setMenu'];
-
 	// 默认的接口文件
 	public $modFile = '';
 	// 默认的图片上传名称
