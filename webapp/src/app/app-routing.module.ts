b0fe960fdf5bc40a66e093010ba548a9591c2e4c
刘亮 <oncwnuCt8t9Jz9-jml3sApX8MY-M@git.weixin.qq.com>
2020-05-20 13:09:07 +0800

    前端hash路由

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import {Routes, RouterModule} from '@angular/router';
 const routes: Routes = [
     {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
     {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
-    {path: '', pathMatch: 'full', redirectTo: 'login'},
+    {path: '', pathMatch: 'full', redirectTo: 'home'},
 
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes, {
-        useHash: false,
+        useHash: true,
         enableTracing: true,
     })],
     exports: [RouterModule]
