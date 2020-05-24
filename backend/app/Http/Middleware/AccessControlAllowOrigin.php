5f0fd0ce65ef7dd69aca580ddeb5fb664260f632
cscsb <49179157+cscsb@users.noreply.github.com>
2020-05-24 15:10:01 +0800

    Update AccessControlAllowOrigin.php

--- backend/app/Http/Middleware/AccessControlAllowOrigin.php
+++ backend/app/Http/Middleware/AccessControlAllowOrigin.php
@@ -20,9 +20,9 @@ class AccessControlAllowOrigin
         $response = $next($request);
         $origin   = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
         $response->headers->add(['Access-Control-Allow-Origin' => $origin]);
-        $response->headers->add(['Access-Control-Allow-Headers' => 'Origin, Content-Type,Pragma,cache-control,x-requested-with, Cookie,X-CSRF-TOKEN, Accept,Authorization,uuid']);
+        $response->headers->add(['Access-Control-Allow-Headers' => 'Origin, Authorization,Token,Content-Type,Pragma,cache-control,x-requested-with,Origin,  Cookie,X-CSRF-TOKEN, Accept,Authorization,uuid']);
         $response->headers->add(['Access-Control-Expose-Headers' => 'Authorization,authenticated']);
-        $response->headers->add(['Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, OPTIONS']);
+        $response->headers->add(['Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, OPTIONS,UPDATE']);
         $response->headers->add(['Access-Control-Allow-Credentials' => 'true']);
         return $response;
     }
