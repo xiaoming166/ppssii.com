09d434538cce80a3714b86576f02f3c3876715eb
cscsb <49179157+cscsb@users.noreply.github.com>
2020-05-24 08:06:01 +0800

    Update Kernel.php

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -17,6 +17,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+           \App\Http\Middleware\PreflightResponse::class,
+
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
